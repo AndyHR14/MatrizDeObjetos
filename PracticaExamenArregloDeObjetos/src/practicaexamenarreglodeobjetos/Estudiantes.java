cca81580d337fec0ad9efc7ec12cb51e18f33415
Sv02v <Se02vas@gmail.com>
2024-08-14 15:28:40 -0600

    Update Estudiantes.java

--- PracticaExamenArregloDeObjetos/src/practicaexamenarreglodeobjetos/Estudiantes.java
+++ PracticaExamenArregloDeObjetos/src/practicaexamenarreglodeobjetos/Estudiantes.java
@@ -12,13 +12,16 @@ public class Estudiantes {
     private String nombre;
     private int edad;
     private String curso;
+    //Se establecen los atributos de la clase 
 
     public Estudiantes(String nombre, int edad, String curso) {
         this.nombre = nombre;
         this.edad = edad;
         this.curso = curso;
+        //Se Crean los constrtuctores 
     }
 
+    //Se crean los sets n gets 
     public String getNombre() {
         return nombre;
     }
